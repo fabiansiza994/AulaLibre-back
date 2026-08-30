6bb7dc44faf72dc2d323f7cad41826976f67d527
fabian mauricio siza paladines <fabiansiza994@gmail.com>
2026-08-30 15:47:11 -0500

    commit inicial

---
+++ src/main/java/com/alulalibre/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.alulalibre.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
